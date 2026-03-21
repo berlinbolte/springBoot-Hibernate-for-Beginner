a4d0e5990a2a00af7bad14d022e585620837c542
Aniket Rathode <aniketrathode405@gmail.com>
2026-03-21 11:21:19 +0530

    Springboot repo is setup

--- 03-SpringBoot-JPA-Hibernate-CRUD/01-CrudDemo-Student/src/main/java/com/berlincode/CrudDemo/CrudDemoApplication.java
+++ 03-SpringBoot-JPA-Hibernate-CRUD/01-CrudDemo-Student/src/main/java/com/berlincode/CrudDemo/CrudDemoApplication.java
@@ -1,5 +1,7 @@
 package com.berlincode.CrudDemo;
 
+import com.berlincode.CrudDemo.dao.StudentDAO;
+import com.berlincode.CrudDemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,22 @@ public class CrudDemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner ->{
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+//		create the student object
+		System.out.println("Creating new Student object ...... ");
+		Student tempStudent = new Student("Berlin ", "Marquina" , "berlinmarquina@gmail.com");
+
+//		save the student object
+		System.out.println("Saving the newly created student object....");
+		studentDAO.save(tempStudent);
+
+//		display id of the saved student
+		System.out.println("Saved Student. Generated id:" + tempStudent.getId());
 	}
 }
---
+++ 03-SpringBoot-JPA-Hibernate-CRUD/01-CrudDemo-Student/src/main/java/com/berlincode/CrudDemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.berlincode.CrudDemo.dao;
+
+import com.berlincode.CrudDemo.entity.Student;
+
+public interface StudentDAO {
+    void save (Student theStudent);
+}
